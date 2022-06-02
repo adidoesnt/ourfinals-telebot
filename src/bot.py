3954cfac64f931a1d3c447cced2db0fc36c5e3bd
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-06-03 02:06:15 +0300

    change default port

--- src/bot.py
+++ src/bot.py
@@ -402,4 +402,4 @@ def webhook():
     return "!", 200
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('port', 8080)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('port', 5000)))
